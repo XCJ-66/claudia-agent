75f2882d2c62b64636ea010768c7657faa6b3f1e
John Downs <jcx7816@gmail.com>
2026-04-22 14:26:43 -0400

    Add ElevenLabs tts.js

--- api/tts.js
+++ api/tts.js
@@ -19,11 +19,11 @@ export default async function handler(req, res) {
         },
         body: JSON.stringify({
           text,
-          model_id: "eleven_monolingual_v1",
+          model_id: "eleven_turbo_v2",
           voice_settings: {
-            stability: 0.75,
-            similarity_boost: 0.85,
-            style: 0.5,
+            stability: 0.45,
+            similarity_boost: 0.90,
+            style: 0.65,
             use_speaker_boost: true,
           },
         }),
