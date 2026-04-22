ae9d4d8df2cb0ad7358cb79023d156526a7feab2
John Downs <jcx7816@gmail.com>
2026-04-22 14:58:34 -0400

    Switch to monolingual model

--- api/tts.js
+++ api/tts.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
         },
         body: JSON.stringify({
           text,
-          model_id: "eleven_turbo_v2",
+          model_id: "eleven_monolingual_v1",
           voice_settings: {
             stability: 0.45,
             similarity_boost: 0.90,
